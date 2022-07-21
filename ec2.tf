ec065818eb7571139e966e0cd96e86db78bfb57d
sathyarajdurai <sathyaraj.durai@cloudreach.com>
2022-07-21 13:29:40 +0530

    terraform ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "ansible_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  # security group
+  vpc_security_group_ids = [aws_security_group.ansible_server.id]
+  # key-pair
+  key_name = "talent-academy-lab"
+  # subnet
+  subnet_id = data.aws_subnet.public.id
+  # EIP
+
+  tags = {
+    Name = "Ansible-Server"
+  }
+}
+
+resource "aws_eip" "ansible_server_ip" {
+  instance = aws_instance.ansible_server.id
+  vpc      = true
+}
